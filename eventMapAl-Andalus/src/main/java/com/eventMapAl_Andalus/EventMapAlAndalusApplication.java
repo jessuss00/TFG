dbe5e6263cd8b5c4e65f1d7dd85fd1e522c93e1c
jessuss00 <jesusmadro23@gmail.com>
2025-12-05 13:38:28 +0100

    spring

---
+++ eventMapAl-Andalus/src/main/java/com/eventMapAl_Andalus/EventMapAlAndalusApplication.java
@@ -0,0 +1,13 @@
+package com.eventMapAl_Andalus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMapAlAndalusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMapAlAndalusApplication.class, args);
+	}
+
+}
